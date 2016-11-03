8ae0feb9eb948f3bf74a877f375e86fda2850d3b
maurisadan <mauri2design@gmail.com>
2016-11-03 09:01:44 +0200

    הוספת הקבצים הקיימים
    html, css, js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var boxchecked = document.getElementsByClassName('weekday');
+var eachday = document.getElementsByName;
+//var showme = document.getElementsByClassName('hide');
+function OnChangeCheckbox(checkbox) {
+	'use strict';
+	for(var i = 0; i< boxchecked.length; i++){
+		if (boxchecked[i].checked.value == ) {
+		console.log(boxchecked[i].value);
+		}
+	}
+}
+
+
+
